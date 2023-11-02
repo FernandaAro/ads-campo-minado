f3fb27c6b38f8f8c8772b58b744d1c8ead92e1a1
Fernanda Aro <fernanda_aro@yahoo.com>
2023-11-02 19:12:04 -0300

    build: finish

--- vscode/Program.cs
+++ vscode/Program.cs
@@ -7,77 +7,138 @@ namespace campoMinado
         public static void Main(string[] args)
         {
             //Criação do campo
-            int[,] campo = new int[10, 10];
-            int[,] jogo = new int[10, 10];
+            int[,] campo = new int[10, 10]; //matriz com as posições do campo
+            int[,] jogo = new int[10, 10]; //matriz que registra as ações do jogador
 
             int qtdLinhas = campo.GetLength(0);
             int qtdColunas = campo.GetLength(1);
 
-            for (int l = 0; l < qtdLinhas; l++)
+            bool problemaArquivo = false;
+            string caminho_absoluto = "C:\\Users\\ferna\\Documents\\dev\\graduation-campo-minado\\vscode\\campo.txt";
+
+            try
             {
-                for (int c = 0; c < qtdColunas; c++)
+                //Informa o caminho e o nome do arquivo
+                StreamReader sr = new StreamReader(caminho_absoluto);
+
+                //Lê a primeira linha
+                String linha_arq = sr.ReadLine();
+                int linha_mtz = 0;
+                int coluna_mtz = 0;
+
+                //Lê até não identificar nova linha
+                while (linha_arq != null || linha_mtz < 10)
                 {
-                    campo[l, c] = 0;
-                    jogo[l, c] = -1;
+                    //Separando os elementos por vírgula
+                    foreach (var numero in linha_arq.Split(','))
+                    {
+                        int num;
+
+                        //Converte os elementos para int
+                        if (int.TryParse(numero, out num))
+                        {
+                            //Armazena elementos na matriz campo
+                            campo[linha_mtz, coluna_mtz] = num;
+                            jogo[linha_mtz, coluna_mtz] = -1;
+                            coluna_mtz++;
+                        }
+                    }
+
+                    //Lê a próxima linha
+                    linha_arq = sr.ReadLine();
+
+                    //Continua a leitura da próxima linha iniciando pelo primeiro valor
+                    coluna_mtz = 0;
+                    linha_mtz++;
                 }
+                //Conclui a leitura do arquivo
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ocorreu um problema na leitura do arquivo!");
+                problemaArquivo = true;
             }
 
-            //Posicionando a bandeira aleatóriamente
-            Random gerador = new Random();
-            int linha = gerador.Next(qtdLinhas);
-            int coluna = gerador.Next(qtdColunas);
-            campo[linha, coluna] = 2;
-
-            //Posicionando as bombas aleatóriamente
-            int bombasPosicionadas = 0;
-            do
+            //Jogo
+            if (!problemaArquivo)
             {
-                linha = gerador.Next(qtdLinhas);
-                coluna = gerador.Next(qtdColunas);
-                if (campo[linha, coluna] == 0)
+                bool fimJogo = false;
+                bool vitoria = false;
+                do
                 {
-                    campo[linha, coluna] = 1;
-                    bombasPosicionadas++;
-                }
-            } while (bombasPosicionadas < 5);
-
-            //Interação do usuário
-            bool fimJogo = false;
-            do
-            {
-                for (int l = 0; l < qtdLinhas; l++)
-                {
-                    for (int c = 0; c < qtdColunas; c++)
+                    for (int l = 0; l < qtdLinhas; l++)
                     {
-                        Console.Write(string.Format("{0}", jogo[l, c]));
+                        for (int c = 0; c < qtdColunas; c++)
+                        {
+                            Console.Write(string.Format("{0}", jogo[l, c]));
+                        }
+                        Console.Write(Environment.NewLine + Environment.NewLine);
                     }
-                    Console.Write(Environment.NewLine + Environment.NewLine);
-                }
-                Console.Write("Selecione uma linha [1-10]: ");
-                linha = Convert.ToInt32(Console.ReadLine()) - 1;
-                Console.Write("Seleciona uma coluna [1-10]: ");
-                coluna = Convert.ToInt32(Console.ReadLine()) - 1;
+                    Console.WriteLine("Seleciona uma linha [1-10]: ");
+                    int linha = Convert.ToInt32(Console.ReadLine()) - 1;
+                    Console.WriteLine("Selecione uma coluna [1-10]: ");
+                    int coluna = Convert.ToInt32(Console.ReadLine()) - 1;
 
-                switch (campo[linha, coluna])
+                    switch (campo[linha, coluna])
+                    {
+                        case 0:
+                            jogo[linha, coluna] = 0;
+                            Console.Write("Continue tentando... \n\n");
+                            break;
+                        case 1:
+                            jogo[linha, coluna] = 1;
+                            Console.Write("BOOOM!!! Você perdeu...\n\n");
+                            fimJogo = true;
+                            break;
+                        default:
+                            jogo[linha, coluna] = 2;
+                            Console.Write("Parabéns, você ganhou!!! \n\n");
+                            fimJogo = true;
+                            vitoria = true;
+                            break;
+                    }
+                } while (!fimJogo);
+
+                string[] arquivo = File.ReadAllLines(caminho_absoluto);
+                string msgVitorias = arquivo[arquivo.Length - 2];
+                string msgDerrotas = arquivo[arquivo.Length - 1];
+                try
                 {
-                    case 0:
-                        jogo[linha, coluna] = 0;
-                        Console.WriteLine("Continue tentando.\n\n");
-                        break;
+                    StreamWriter sw = new StreamWriter(caminho_absoluto);
+                    int contagem;
+                    int linha_sobrescrever;
+                    string texto;
 
-                    case 1:
-                        jogo[linha, coluna] = 1;
-                        Console.WriteLine("BOOM!!! Você perdeu...\n\n");
-                        fimJogo = true;
-                        break;
+                    if (vitoria)
+                    {
+                        int.TryParse(msgVitorias.Split(':')[1], out contagem);
+                        linha_sobrescrever = 12;
+                        texto = "Vitórias:";
+                    }
+                    else
+                    {
+                        int.TryParse(msgDerrotas.Split(':')[1], out contagem);
+                        linha_sobrescrever = 13;
+                        texto = "Derrotas:";
+                    }
 
-                    default:
-                        jogo[linha, coluna] = 2;
-                        Console.WriteLine("Parabéns!!! Você ganhou! \n\n");
-                        fimJogo = true;
-                        break;
+                    contagem++;
+
+                    for (int i = 0; i < arquivo.Length; i++)
+                    {
+                        if (i == linha_sobrescrever)
+                            sw.WriteLine(texto + contagem);
+                        else
+                            sw.WriteLine(arquivo[i]);
+                    }
+                    sw.Close();
                 }
-            } while (!fimJogo);
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ocorreu um problema na escrita do arquivo...");
+                }
+            }
         }
     }
 }
